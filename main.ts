52fbd3c2344784b0ba90c2dd3dd4f206dab2d67f
mgoikoetxeam <122541363+mgoikoetxeam@users.noreply.github.com>
2023-01-12 15:11:19 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,61 @@
+input.onGesture(Gesture.Shake, function () {
+    Dado += randint(1, 6)
+})
+basic.showIcon(IconNames.Square)
+let Dado = 0
+basic.forever(function () {
+    if (Dado == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (Dado == 2) {
+        basic.showLeds(`
+            . . . . .
+            . . . # .
+            . . . . .
+            . # . . .
+            . . . . .
+            `)
+    }
+    if (Dado == 3) {
+        basic.showLeds(`
+            . . . . .
+            . . . # .
+            . . # . .
+            . # . . .
+            . . . . .
+            `)
+    }
+    if (Dado == 4) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            `)
+    }
+    if (Dado == 5) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . # . .
+            . # . # .
+            . . . . .
+            `)
+    }
+    if (Dado == 6) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . # . # .
+            . # . # .
+            . . . . .
+            `)
+    }
+})
